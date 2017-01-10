7b8c14692b784ec9827e07d7ac97c64d5bfd5716
zengxs <zengxs@yonyou.com>
2017-01-10 09:25:58 +0800

    增加文件。

---
+++ 微博文档/[病理知识]血小板减少性紫癜（ITP）知识问答_files/avatar_2.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html><html><head><meta charset="utf-8"><title>小云APP</title><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"><meta name="apple-mobile-web-app-title" content="小云APP"><meta name="apple-mobile-web-app-capable" content="yes"><meta name="mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"><meta name="format-detection" content="telephone=no"><meta name="description" content="小云APP"><style>html, body, .page-screen {
+    height: 100%;
+    font-family: "Helvetica Neue",Helvetica,sans-serif;
+}
+html, body {
+    background: #efeff4;
+}
+* {
+    margin: 0;
+    padding: 0;
+}
+a {
+    text-decoration: none;
+    outline: none;
+}
+
+.page-screen {
+    max-width: 420px;
+    position: relative;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100%;
+}
+.page-screen-icons-list {
+    position: absolute;
+    overflow: hidden;
+    width: 100%;
+    text-align: center;
+        bottom: 65px;
+}
+.page-screen-icon-box {
+    display: inline-block;
+    width: 25%;
+}
+.page-screen-item-icon {
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    margin: 5px auto;
+    box-shadow: 0px 0px 5px #ccc;
+}
+.page-screen-item-title {
+    font-size: 12px;
+    color: #666;
+}
+.page-screen-title {
+    position: absolute;
+    top: 20%;
+    text-align: center;
+    left: 0;
+    right: 0;
+    font-size: 72px;
+    color: #666;
+    font-weight: bold;
+}
+.page-screen-desc {
+    position: absolute;
+    bottom: 20px;
+    text-align: center;
+    left: 0;
+    right: 0;
+    font-size: 16px;
+    color: #666;
+}
+.page-group {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #efeff4;
+}
+@media screen and (min-width: 420px) {
+    .page-group {
+        left: 50%;
+        margin-left: -210px;
+    }
+}</style></head><body><div class="page-group"><div class="page-screen" style="background-image: url(http://cdn-wz.xiaoyun.com/m/img/screen/xiaoyun_app_screen.png)"><div class="page-screen-title">小云微站</div><div class="page-screen-desc">用小云APP轻松实现属于自己的APP</div><div class="page-screen-icons-list"><a class="page-screen-icon-box" href="//41961.xiaoyun.com/m"><div class="page-screen-item-icon" style="background-image: url(http://cdn-wz.xiaoyun.com/m/f/http%253A%252F%252Fimg.appbyme.com%252Fd%252Fimg%252Faca%252Ficon%252F2016%252F03%252F02%252Foriginal%252F2748bcaa-1e79-4fe3-a456-d81cc33e94e2.png)"></div><div class="page-screen-item-title">新昌信息港</div></a><a class="page-screen-icon-box" href="//114714.xiaoyun.com/m"><div class="page-screen-item-icon" style="background-image: url(http://cdn-wz.xiaoyun.com/m/f/http%253A%252F%252Fimg.appbyme.com%252Fd%252Fimg%252Faca%252Ficon%252F2016%252F03%252F27%252Foriginal%252Fd78dd24c-f9f8-47d4-a793-ed35d320946d.png)"></div><div class="page-screen-item-title">水果邦</div></a><a class="page-screen-icon-box" href="//111687.xiaoyun.com/m"><div class="page-screen-item-icon" style="background-image: url(http://cdn-wz.xiaoyun.com/m/f/http%253A%252F%252Fimg.appbyme.com%252Fd%252Fimg%252Faca%252Ficon%252F2016%252F02%252F04%252Foriginal%252F99ffe002-139a-4088-b893-4a99a279e211.png)"></div><div class="page-screen-item-title">江油都市网</div></a><a class="page-screen-icon-box" href="//18894.xiaoyun.com/m"><div class="page-screen-item-icon" style="background-image: url(http://cdn-wz.xiaoyun.com/m/f/http%253A%252F%252Fimg.appbyme.com%252Fd%252Fimg%252Faca%252Ficon%252F2016%252F03%252F02%252Foriginal%252F59bcf05b-367e-47ee-85c8-02a48440dea4.png)"></div><div class="page-screen-item-title">淋巴瘤之家</div></a></div></div></div><script>var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?64144a57b6d06ecc940114a0c8d2d3a5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();</script></body></html>
